a2061fd902fdea1f78181b0feeb0dba9f9a7d211
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-04-26 21:00:14 +0530

    Power outage- add commment on services

--- src/com/Poweroutageservice.java
+++ src/com/Poweroutageservice.java
@@ -12,7 +12,13 @@ import org.jsoup.nodes.Document;
 
 @Path("/Bill")
 public class Poweoutageservice {
+	Poweroutage PoweroutageObj = new Poweroutage();
 
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readPoweroutage() {
+		return PoweroutageObj.readPoweroutag();
 	}
 
 	
@@ -37,14 +43,14 @@ public class Poweoutageservice {
 	@Produces(MediaType.TEXT_PLAIN)
 
 	public String updatePoweroutage(String Poweroutageob) {
-
+		// Convert the input string to a JSON object
 		JsonObject ProObject = new JsonParser().parse(Poweroutageob).getAsJsonObject();
 
+		// Read the values from the JSON object
 		String Id = ProObject.get("id").getAsString();
 		String date = ProObject.get("date").getAsString();
 		String time = ProObject.get("time").getAsString();
 		String areacode = ProObject.get("areacode").getAsString();
-
 		String output = PoweroutageObj.updatePoweroutag(time, date, areacode);
 		return output;
 	}
@@ -55,10 +61,10 @@ public class Poweoutageservice {
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String deleteBill(String PoweroutageOb) {
-
+		// Convert the input string to an XML document
 		Document doc = Jsoup.parse(PoweroutageOb, "", Parser.xmlParser());
 
-
+		// Read the value from the element <itemID>
 		String Id = doc.select("id").text();
 		String output = PoweroutageObj.deletePoweroutag(Id);
 		return output;
