d8f69df1055c884e81e8e34e28cd58a43ed28a93
Denura Mudalige <87606831+MudaligeDLM@users.noreply.github.com>
2022-04-26 17:59:18 +0530

    customer create

---
+++ target/classes/com/Customerservice.java
@@ -0,0 +1,39 @@
+package com;
+
+import model.Employee;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import com.google.gson.*;
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Employee")
+public class Customerservice {
+	Employee EmployeeObj = new Employee();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readEmployee() {
+		return EmployeeObj.readEmployee();
+	}
+
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertEmployee(@FormParam("name") String Name, 
+			@FormParam("age") String age,
+			@FormParam("address") String address) {
+		String output = EmployeeObj.insertEmployee(Name, age, address);
+		return output;
+
+	}
+
+	
+	
+}
---
+++ target/classes/model/Customer.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Customer {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3309/empmanagment?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertCustomer(String name, String billno, String address) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			
+		return output;
+	}
+
+	
+	
+}
