b25d1919e13ba0aee19c9fe8eb82e14d8e542354
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-04-26 20:30:58 +0530

    Power Outage- implemented create part

---
+++ src/model/Poweroutage.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Poweroutage {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/empmanagment?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String inserPoweroutage(String time, String date, String empid ,  String areacode) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into poweroutages(`time`, `date`,   `empid`, `areaid`)" + " values ( ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, time);
+			preparedStmt.setString(3, date);
+			preparedStmt.setString(4, empid);
+			preparedStmt.setString(5, areacode);
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the Poweroutage Shedule.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+}
