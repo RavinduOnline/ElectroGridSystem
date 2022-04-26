77a81085f99a3a9c9ac4fd7b117c95854ddb32ca
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-04-26 20:37:17 +0530

    power outage- created service file

---
+++ src/com/Poweroutageservice.java
@@ -0,0 +1,46 @@
+package com;
+
+import model.Poweroutage;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import com.google.gson.*;
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Bill")
+public class Poweoutageservice {
+
+	}
+
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertPoweroutage(@FormParam("date") String date, 
+			@FormParam("time") String time, 
+			@FormParam("time") String empid, 
+			@FormParam("cusid") String areacode) {
+		String output = PoweroutageObj.inserPoweroutage(time, date, empid, areacode);
+		return output;
+
+	}
+	
+	
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+
+	public String updatePoweroutage(String Poweroutageob) {
+		
+	}
+
+
+
+	
+}
